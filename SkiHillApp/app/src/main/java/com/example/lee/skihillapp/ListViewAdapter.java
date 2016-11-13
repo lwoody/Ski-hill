b869e3cd51be0f21ddb560cd2300a5d89d2fcab8
lwoody <lwoody@korea.ac.kr>
2016-11-13 10:48:57 -0700

    documentation changed

--- SkiHillApp/app/src/main/java/com/example/lee/skihillapp/ListViewAdapter.java
+++ SkiHillApp/app/src/main/java/com/example/lee/skihillapp/ListViewAdapter.java
@@ -16,41 +16,41 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ListViewAdapter extends BaseAdapter {
-    // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
+    // ArrayList for adding data in Adapter
     private ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>() ;
 
-    // ListViewAdapter의 생성자
+    // ListViewAdapter generator
     public ListViewAdapter() {
 
     }
 
-    // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
+    // return the number of data in Adapter
     @Override
     public int getCount() {
         return listViewItemList.size() ;
     }
 
-    // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
+    // displaying data to View in position
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         final int pos = position;
         final Context context = parent.getContext();
 
-        // "listview_item" Layout을 inflate하여 convertView 참조 획득.
+        // "listview_item" Layout inflate then convertView reference retained
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.listview_item, parent, false);
         }
 
-        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+        // Widget retained from View
         ImageView iconImageView = (ImageView) convertView.findViewById(R.id.imageView1) ;
         TextView titleTextView = (TextView) convertView.findViewById(R.id.textView1) ;
         TextView descTextView = (TextView) convertView.findViewById(R.id.textView2) ;
 
-        // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
+        // data reference in position from Data Set(listViewItemList)
         ListViewItem listViewItem = listViewItemList.get(position);
 
-        // 아이템 내 각 위젯에 데이터 반영
+        // apply the data to widget
         iconImageView.setImageDrawable(listViewItem.getIcon());
         titleTextView.setText(listViewItem.getTitle());
         descTextView.setText(listViewItem.getDesc());
@@ -58,19 +58,19 @@ public class ListViewAdapter extends BaseAdapter {
         return convertView;
     }
 
-    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
+    // row id return from each position
     @Override
     public long getItemId(int position) {
         return position ;
     }
 
-    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
+    // data return from each position
     @Override
     public Object getItem(int position) {
         return listViewItemList.get(position) ;
     }
 
-    // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
+    // function for adding item data
     public void addItem(Drawable icon, String title, String desc) {
         ListViewItem item = new ListViewItem();
 
