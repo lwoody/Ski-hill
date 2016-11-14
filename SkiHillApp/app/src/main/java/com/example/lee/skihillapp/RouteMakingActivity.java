de6b2fe4794dc1c33e4f027a14240d8309e9f722
brianisbae <asianese.brian@hotmail.com>
2016-11-13 18:41:55 -0700

    fdkslafdla

--- SkiHillApp/app/src/main/java/com/example/lee/skihillapp/RouteMakingActivity.java
+++ SkiHillApp/app/src/main/java/com/example/lee/skihillapp/RouteMakingActivity.java
@@ -4,9 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.view.View;
+import android.widget.ViewSwitcher;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
 
 public class RouteMakingActivity extends AppCompatActivity {
 
+    int imageNo[]={R.drawable.lakelouise_overview_1,R.drawable.lakelouise_overview_2,R.drawable.lakelouise_overview_3,R.drawable.lakelouise_overview_4,R.drawable.lakelouise_overview_5};
+
+    int countImage = imageNo.length;
+    int currentImage = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +32,31 @@ public class RouteMakingActivity extends AppCompatActivity {
         String title = intent.getStringExtra("title");
         this.setTitle(title);
 
+        Button nextImage=(Button)findViewById(R.id.btnNext);
+        final ImageSwitcher imageSwitcher = (ImageSwitcher) findViewById(R.id.imageChanger);
+
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                return imageView;
+
+            }
+        });
+
+        nextImage.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                currentImage++;
+
+                if(currentImage==countImage)
+                    countImage=0;
+
+                imageSwitcher.setImageResource(imageNo[currentImage]);
+
+            }
+
+        });
     }
 
 }
