753aaf62bec9990994715637c7f900b018a51acb
CJFulford <cjfulford@shaw.ca>
2016-11-16 18:57:43 -0700

    added activities for each lift. linked buttons on route maker page to their respective runs

---
+++ LakeLouiseApp/app/src/main/java/com/example/lee/lakelouiseapp/grizzly_runs.java
@@ -0,0 +1,13 @@
+package com.example.lee.lakelouiseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class grizzly_runs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grizzly_runs);
+    }
+}
