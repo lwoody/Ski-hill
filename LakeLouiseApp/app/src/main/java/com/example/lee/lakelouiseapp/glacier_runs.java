769aecd033a98aa0718b0c67463af1663613ba23
CJFulford <cjfulford@shaw.ca>
2016-11-16 19:12:59 -0700

    added button functions to glacier page and linked buttons to functions. Buttons currently do nothing

--- LakeLouiseApp/app/src/main/java/com/example/lee/lakelouiseapp/glacier_runs.java
+++ LakeLouiseApp/app/src/main/java/com/example/lee/lakelouiseapp/glacier_runs.java
@@ -1,7 +1,9 @@
 package com.example.lee.lakelouiseapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class glacier_runs extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class glacier_runs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glacier_runs);
     }
+
+    public void run_2Button(View v){}
+    public void run_3Button(View v){}
+    public void run_4Button(View v){}
+    public void run_5Button(View v){}
+    public void run_6Button(View v){}
+    public void run_7Button(View v){}
+    public void run_8Button(View v){}
+    public void run_9Button(View v){}
+    public void run_10Button(View v){}
+    public void run_11Button(View v){}
+    public void run_12Button(View v){}
+    public void run_13Button(View v){}
+    public void run_14Button(View v){}
 }
