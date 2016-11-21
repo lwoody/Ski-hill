04112d3d302d9d37efcc1947df2472c06b0164aa
lwoody <lwoody@korea.ac.kr>
2016-11-20 21:42:40 -0700

    route view done

--- LakeLouiseApp/app/src/main/java/com/example/lee/lakelouiseapp/SelectRouteActivity.java
+++ LakeLouiseApp/app/src/main/java/com/example/lee/lakelouiseapp/SelectRouteActivity.java
@@ -1,7 +1,9 @@
 package com.example.lee.lakelouiseapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SelectRouteActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SelectRouteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_route);
+    }
+
+    public void onClickSubmit(View v){
+        startActivity(new Intent(this, RouteShowActivity.class));
     }
 }
