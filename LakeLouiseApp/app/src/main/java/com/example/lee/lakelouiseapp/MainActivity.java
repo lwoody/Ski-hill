92e5da55cbc8c29644a8b12c1fb6023a7ef21d4f
lwoody <lwoody@korea.ac.kr>
2016-11-30 02:18:25 -0700

    design and function changed!

--- LakeLouiseApp/app/src/main/java/com/example/lee/lakelouiseapp/MainActivity.java
+++ LakeLouiseApp/app/src/main/java/com/example/lee/lakelouiseapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -12,6 +13,8 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,31 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
         this.setTitle("");
 
-//        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
-//
-//        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
-//        bottomBar.setItemsFromMenu(R.menu.bottom_button, new OnMenuTabSelectedListener() {
-//            @Override
-//            public void onMenuItemSelected(int itemId) {
-//                switch (itemId) {
-//                    case R.id.route_item:
-//                        Snackbar.make(coordinatorLayout, "Recent Item Selected", Snackbar.LENGTH_LONG).show();
-//                        break;
-//                    case R.id.info_item:
-//                        Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
-//                        break;
-//                }
-//            }
-//        });
-//
-//        // Set the color for the active tab. Ignored on mobile when there are more than three tabs.
-//        bottomBar.setActiveTabColor("#C2185B");
-
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
+
         final RelativeLayout mainContent1 = (RelativeLayout) findViewById(R.id.MainContent1);
+        final RelativeLayout mainContent2 = (RelativeLayout) findViewById(R.id.MainContent2);
 
-        mainContent1.setVisibility(View.VISIBLE);
+        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItemsFromMenu(R.menu.bottom_button, new OnMenuTabSelectedListener() {
+            @Override
+            public void onMenuItemSelected(int itemId) {
+                switch (itemId) {
+                    case R.id.route_item:
+                        mainContent1.setVisibility(View.VISIBLE);
+                        mainContent2.setVisibility(View.INVISIBLE);
+                        Snackbar.make(coordinatorLayout, "Recent Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.info_item:
+                        mainContent2.setVisibility(View.VISIBLE);
+                        mainContent1.setVisibility(View.INVISIBLE);
+                        Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
 
+        // Set the color for the active tab. Ignored on mobile when there are more than three tabs.
+        bottomBar.setActiveTabColor("#4259f4");
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
